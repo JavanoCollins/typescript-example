59d00bfa68a43a37e1753813cecaaa1d7990eb2a
Resume Website <jdcollins242@gmail.com>
2020-08-16 21:17:13 -0400

    removed JavaScript files

--- app.js
+++
@@ -1,4 +0,0 @@
-// var multiply = function (num1, num2) { return num1 * num2; };
-// var example1 = multiply('2', 2); // Invalid in TypeScript but compiles to 44 in JavaScript
-// var example2 = multiply(2, 2); // Compiles to 4
-// console.log(example1);
